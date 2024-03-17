f14deb111292853db6433cb6c8d7d37b1230f8a7
Hatice İKKAN <58442650+Haticeikkan@users.noreply.github.com>
2024-03-18 02:01:39 +0300

    GBFF file download and convert to .fasta .
    The requested file was downloaded from the link and convert to .fasta format.

---
+++ Convert_to_fasta.py
@@ -0,0 +1,24 @@
+import gzip
+from Bio import SeqIO
+from urllib.request import urlopen
+
+# Dosyayı İndir
+url = "https://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi/fungi.28SrRNA.gbff.gz"
+output_file = "28S_rRNA.fasta"
+
+try:
+    with urlopen(url) as response, open("fungi.28SrRNA.gbff.gz", 'wb') as out_file:
+        out_file.write(response.read())
+    print("Dosya indirildi ve kaydedildi.")
+except Exception as e:
+    print("Hata:", e)
+
+# Dosyayı aç
+with gzip.open("fungi.28SrRNA.gbff.gz", 'rt') as gbff_file:
+    # Ayrıştır the GBFF file
+    records = SeqIO.parse(gbff_file, "genbank")
+    
+    # Veriyi FASTA formatına dönüştür
+    SeqIO.write(records, output_file, "fasta")
+
+print(f"Veri {output_file} dosyasına FASTA formatında dönüştürüldü.")
